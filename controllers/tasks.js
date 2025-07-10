77985cbfd3ce3ae4b2286426f15e0b4256424ab5
jirighoemaanuel <emmaweyinmi@gmail.com>
2025-07-10 11:17:12 +0100

    update

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,12 @@
+import Task from '../models/Tasks.js';
+
 const getAllTasks = (req, res) => {
   res.send('get all tasks');
 };
 
-const createTask = (req, res) => {
-  res.json(req.body);
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
 };
 
 const getTask = (req, res) => {
