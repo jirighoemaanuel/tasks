1835e61869589c65420c81f5e3cc2ff8e8f22ac4
jirighoemaanuel <emmaweyinmi@gmail.com>
2025-07-10 10:30:52 +0100

    update

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import tasksRouter from './routes/tasks.js';
 const app = express();
 import connectDB from './db/connect.js';
 
+const connectionString = process.env.MONGO_URL;
 const port = 3000;
 
 // middleware
@@ -19,7 +20,7 @@ app.use('/api/v1/tasks', tasksRouter);
 
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(connectionString);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {}
 };
